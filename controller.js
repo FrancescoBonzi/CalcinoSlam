3421cd0a6097432359b09255714492ca2053c14c
FrancescoBonzi <fra.bonzi@gmail.com>
2021-12-26 15:00:09 +0100

    generate_championship added

--- controller.js
+++ controller.js
@@ -11,12 +11,15 @@ function error500(err, req, res, next) {
     res.render('500');
 }
 
-function home_page(req, res, next) {
+function create_championship(req, res, next) {
+    players = req.body
+    championship = await model.create_championship(players)
+    // send back tournaments
     return 0
 }
 
 exports.dispatcher = function (app) {
-    app.get('/', home_page);
+    app.post('/', create_championship);
     app.use(error404); // 404 catch-all handler (middleware)
     app.use(error500); // 500 error handler (middleware)
 }
---
+++ model.js
@@ -0,0 +1,3 @@
+function create_championship(players) {
+    // ex players = [0, 1, 2, 3, 4, 6, 9, 12]
+}
